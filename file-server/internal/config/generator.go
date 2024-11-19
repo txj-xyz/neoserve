aadba24816ef58d7e1759c037f9735b009043a57
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-11-19 09:58:01 -0700

    i love this game

--- file-server/internal/config/generator.go
+++ file-server/internal/config/generator.go
@@ -6,14 +6,27 @@ import (
 )
 
 /*
-	Generates a random name that is 16 bytes
+Generates a random name that is 16 bytes
 
-	@returns [string]
+@returns [string]
 */
-func GenerateRandomName() (string) {
+func GenerateRandomName() string {
 	randomBytes := make([]byte, 16)
 	_, err := rand.Read(randomBytes)
-	if err != nil { return "" }
+	if err != nil {
+		return ""
+	}
 	r := hex.EncodeToString(randomBytes)
 	return r
 }
+
+func GenerateName(prefix string) string {
+	rb := make([]byte, 8)
+	_, err := rand.Read(rb)
+	if err != nil {
+		return ""
+	}
+	r := prefix + hex.EncodeToString(rb)
+	return r
+}
+
